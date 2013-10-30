13053df83d889bcd7354577232a98fcfb6ff24db
Anurag Goel <anurag@nimbuzz.com>
2013-10-30 10:20:06 +0530

    Anurag: modified login code

--- FacebookAlbum/src/com/example/facebookalbum/MainActivity.java
+++ FacebookAlbum/src/com/example/facebookalbum/MainActivity.java
@@ -1,79 +1,28 @@
 package com.example.facebookalbum;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends Activity {
+	private MainFragment mainFragment;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		// start Facebook Login
-		Session.openActiveSession(this, true, new Session.StatusCallback() {
-
-			// callback when session changes state
-			@Override
-			public void call(Session session, SessionState state, Exception exception) {
-				if (session.isOpened()) {
-					// make request to the /me API
-					Request.newMeRequest(session, new Request.GraphUserCallback() {
-
-					  // callback after Graph API response with user object
-					  @Override
-					  public void onCompleted(GraphUser user, Response response) {
-						  if (user != null) {
-							  TextView welcome = (TextView) findViewById(R.id.welcome);
-							  welcome.setText("Hello " + user.getName() + "!");
-							}
-					  }
-					}).executeAsync();
-				}
-			}
-		});
-
-		
-	/*	// Add code to print out the key hash
-		try {
-			PackageInfo info = getPackageManager().getPackageInfo(
-					"com.example.facebookalbum", 
-					PackageManager.GET_SIGNATURES);
-			for (Signature signature : info.signatures) {
-				MessageDigest md = MessageDigest.getInstance("SHA");
-				md.update(signature.toByteArray());
-				Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-				Toast.makeText(getApplicationContext(), "KeyHash:" + Base64.encodeToString(md.digest(), Base64.DEFAULT),Toast.LENGTH_LONG).show();
-
-			}
-		} catch (NameNotFoundException e) {
-
-		} catch (NoSuchAlgorithmException e) {
-
-		}*/
-
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		if(savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			mainFragment = new MainFragment();
+			getSupportFragmentManager().beginTransaction()
+			.add(android.R.id.content, mainFragment).commit();
+		}
+		else {
+			// Or set the fragment from restored state info
+			mainFragment = (MainFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
 	}
 
 }
